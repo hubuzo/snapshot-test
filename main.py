7efa7587169215339800461c2c15ff69218fbd3c
hubuzo <hubuzo@icloud.com>
2023-03-22 23:06:24 +0900

    first commit and shapshot-test

---
+++ main.py
@@ -0,0 +1,18 @@
+from app.screenshot_comparison_test import ScreenshotComparisonTest 
+
+# URLリストを定義する
+urls = [
+    'https://www.google.com/',
+    'https://www.yahoo.co.jp/',
+    'https://www.amazon.com/',
+    'https://www.microsoft.com/',
+]
+
+# ScreenshotComparisonTestクラスを初期化する
+test = ScreenshotComparisonTest(urls)
+
+# スクリーンショットを撮影する
+test.capture_screenshots()
+
+# スクリーンショットを比較する
+test.compare_screenshots()
